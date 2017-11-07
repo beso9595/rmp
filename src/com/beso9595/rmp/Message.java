5cca2c5319a564a9ec18436b6e620e058508089a
Beso Bantsadze <beso.bantsadze@gmail.com>
2017-11-07 16:56:21 +0400

    test

--- src/com/beso9595/rmp/Message.java
+++ src/com/beso9595/rmp/Message.java
@@ -13,7 +13,6 @@ public interface Message {
     public static final String LIST_IS_OVER = "List is over, choose new path or repeat the current";
     public static final String MUST_BE_LESS = "Must be less then non-played songs quantity";
     public static final String EMPTY_DIR = "Not a single *.mp3 format file was found in directory";
-    //public static final String EMPTY_DIR = "Not a single *.mp3 format file was found in directory";
 
     public static final String VERSION = "1.1";
     public static final String WELCOME = "\n" +
